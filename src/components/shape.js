b1bd4e5bfca3294929e55bfec8ea64d6ed895257
Md Amir Sohail <52571788+Armuibry@users.noreply.github.com>
2022-08-10 14:59:52 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,16 +1,18 @@
 // declare class
 class Shape {
     constructor(){
-        this.color = "red";
-    }
-
-    drawShape(){
+        this.color = "red",
+        
+          drawShape(){
         console.log("drawShape");
     }
 
     calculateArea(){
         console.log("drawShape");
     }
+    }
+
+  
 }
 
 module.exports = Shape;

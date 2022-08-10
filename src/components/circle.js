ab0c8efe004a554820fc88e80bba912e3f3bc5ea
Md Amir Sohail <52571788+Armuibry@users.noreply.github.com>
2022-08-10 14:33:51 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,8 +4,8 @@ let Shape = required('./shape.js');
 
 // declare class
 class Circle extends Shape {
-    constructor(){
-
+    constructor(name){
+        this.name = name;
     }
     calculateArea(){
         console.log("overiden method");

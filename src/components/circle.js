8c88355ed0d8b78812e07db072976947c497fd6b
Md Amir Sohail <mdamirsohail1@gmail.com>
2022-08-10 12:53:47 +0530

    class updated

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,18 @@
 // import using require
 
+let shape = required('./shape');
+
 // declare class
+class Circle {
+    constructor(){
+
+    }
+    calculateArea = function(){
+
+    }
+}
+
+module.exports = Circle;
 
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
+class Shape {
+    constructor(color){
+        this.color = color;
+    }
+
+    drawShape = function(){
+
+    }
+
+    calculateArea = function(){
+
+    }
+}
+
+module.exports = Shape;
 
 // export class using module.exports
